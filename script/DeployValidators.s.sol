5cfedc43d3525cca3f72e17d26cdcca962db557b
Cuong Tran <tranquoccuong@outlook.com.vn>
2025-04-15 11:48:33 +0700

    chore: update Base addresses

--- script/DeployValidators.s.sol
+++ script/DeployValidators.s.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import './Base.s.sol';
 
 import 'src/validators/KSPriceBasedDCAIntentValidator.sol';
-import 'src/validators/KSSwapIntentValidator.sol';
 import 'src/validators/KSTimeBasedDCAIntentValidator.sol';
 
 contract DeployValidators is BaseScript {
@@ -22,22 +21,19 @@ contract DeployValidators is BaseScript {
 
     vm.startBroadcast();
 
-    KSSwapIntentValidator swapValidator = new KSSwapIntentValidator();
     KSPriceBasedDCAIntentValidator priceBasedDCAValidator =
       new KSPriceBasedDCAIntentValidator(_toArray(router));
     KSTimeBasedDCAIntentValidator timeBasedDCAValidator =
       new KSTimeBasedDCAIntentValidator(_toArray(router));
 
     string memory path = string(abi.encodePacked(root, '/script/deployedAddresses/'));
-    string[] memory keys = new string[](3);
-    keys[0] = 'swapValidator';
-    keys[1] = 'priceBasedDCAValidator';
-    keys[2] = 'timeBasedDCAValidator';
+    string[] memory keys = new string[](2);
+    keys[0] = 'KSPriceBasedDCAIntentValidator';
+    keys[1] = 'KSTimeBasedDCAIntentValidator';
 
-    address[] memory addresses = new address[](3);
-    addresses[0] = address(swapValidator);
-    addresses[1] = address(priceBasedDCAValidator);
-    addresses[2] = address(timeBasedDCAValidator);
+    address[] memory addresses = new address[](2);
+    addresses[0] = address(priceBasedDCAValidator);
+    addresses[1] = address(timeBasedDCAValidator);
 
     _writeAddress(path, 'validators', chainId, keys, addresses);
 
