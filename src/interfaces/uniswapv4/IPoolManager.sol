8bd00b5d1b467f13a79180362f11d968e8c6a4b3
Vu Nguyen Hoang <nghoangvu24@gmail.com>
2025-05-29 09:41:14 +0700

    feat: conditional zap out uniswap v4

---
+++ src/interfaces/uniswapv4/IPoolManager.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @notice Returns the key for identifying a pool
+struct PoolKey {
+  /// @notice The lower currency of the pool, sorted numerically
+  address currency0;
+  /// @notice The higher currency of the pool, sorted numerically
+  address currency1;
+  /// @notice The pool LP fee, capped at 1_000_000. If the highest bit is 1, the pool has a dynamic fee and must be exactly equal to 0x800000
+  uint24 fee;
+  /// @notice Ticks that involve positions must be a multiple of tick spacing
+  int24 tickSpacing;
+  /// @notice The hooks of the pool
+  address hooks;
+}
+
+/// @notice Interface for the PoolManager
+interface IPoolManager {
+  /// @notice Called by external contracts to access granular pool state
+  /// @param slot Key of slot to sload
+  /// @return value The value of the slot as bytes32
+  function extsload(bytes32 slot) external view returns (bytes32 value);
+
+  /// @notice Called by external contracts to access granular pool state
+  /// @param startSlot Key of slot to start sloading from
+  /// @param nSlots Number of slots to load into return value
+  /// @return values List of loaded values.
+  function extsload(bytes32 startSlot, uint256 nSlots)
+    external
+    view
+    returns (bytes32[] memory values);
+
+  /// @notice Called by external contracts to access sparse pool state
+  /// @param slots List of slots to SLOAD from.
+  /// @return values List of loaded values.
+  function extsload(bytes32[] calldata slots) external view returns (bytes32[] memory values);
+
+  /// @notice Called by external contracts to access transient storage of the contract
+  /// @param slot Key of slot to tload
+  /// @return value The value of the slot as bytes32
+  function exttload(bytes32 slot) external view returns (bytes32 value);
+
+  /// @notice Called by external contracts to access sparse transient pool state
+  /// @param slots List of slots to tload
+  /// @return values List of loaded values
+  function exttload(bytes32[] calldata slots) external view returns (bytes32[] memory values);
+
+  /// @notice Thrown when a currency is not netted out after the contract is unlocked
+  error CurrencyNotSettled();
+
+  /// @notice Thrown when trying to interact with a non-initialized pool
+  error PoolNotInitialized();
+
+  /// @notice Thrown when unlock is called, but the contract is already unlocked
+  error AlreadyUnlocked();
+
+  /// @notice Thrown when a function is called that requires the contract to be unlocked, but it is not
+  error ManagerLocked();
+
+  /// @notice Pools are limited to type(int16).max tickSpacing in #initialize, to prevent overflow
+  error TickSpacingTooLarge(int24 tickSpacing);
+
+  /// @notice Pools must have a positive non-zero tickSpacing passed to #initialize
+  error TickSpacingTooSmall(int24 tickSpacing);
+
+  /// @notice PoolKey must have currencies where address(currency0) < address(currency1)
+  error CurrenciesOutOfOrderOrEqual(address currency0, address currency1);
+
+  /// @notice Thrown when a call to updateDynamicLPFee is made by an address that is not the hook,
+  /// or on a pool that does not have a dynamic swap fee.
+  error UnauthorizedDynamicLPFeeUpdate();
+
+  /// @notice Thrown when trying to swap amount of 0
+  error SwapAmountCannotBeZero();
+
+  ///@notice Thrown when native currency is passed to a non native settlement
+  error NonzeroNativeValue();
+
+  /// @notice Thrown when `clear` is called with an amount that is not exactly equal to the open currency delta.
+  error MustClearExactPositiveDelta();
+}
---
+++ src/validators/KSZapOutUniswapV4IntentValidator.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import './base/BaseIntentValidator.sol';
+import 'openzeppelin-contracts/token/ERC20/IERC20.sol';
+import 'src/interfaces/uniswapv4/IPositionManager.sol';
+import 'src/libraries/StateLibrary.sol';
+
+contract KSZapOutUniswapV4IntentValidator is BaseIntentValidator {
+  using StateLibrary for IPoolManager;
+
+  error InvalidZapOutPosition();
+
+  error OutsidePriceRange(uint160 sqrtPLower, uint160 sqrtPUpper, uint160 sqrtPriceX96);
+
+  error GetPositionLiquidityFailed();
+
+  error GetSqrtPriceX96Failed();
+
+  error BelowMinRate(uint256 liquidity, uint256 minRate, uint256 outputAmount);
+
+  uint256 public constant RATE_DENOMINATOR = 1e18;
+
+  address constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+  struct ZapOutUniswapV4ValidationData {
+    address[] nftAddresses;
+    uint256[] nftIds;
+    address[] pools;
+    address[] outputTokens;
+    uint160[] sqrtPLowers;
+    uint160[] sqrtPUppers;
+    uint256[] minRates;
+    address recipient;
+  }
+
+  modifier checkTokenLengths(IKSSessionIntentRouter.TokenData calldata tokenData) override {
+    require(tokenData.erc20Data.length == 0, InvalidTokenData());
+    require(tokenData.erc721Data.length == 1, InvalidTokenData());
+    require(tokenData.erc1155Data.length == 0, InvalidTokenData());
+    _;
+  }
+
+  /// @inheritdoc IKSSessionIntentValidator
+  function validateBeforeExecution(
+    bytes32,
+    IKSSessionIntentRouter.IntentCoreData calldata coreData,
+    IKSSessionIntentRouter.ActionData calldata actionData
+  )
+    external
+    view
+    override
+    checkTokenLengths(actionData.tokenData)
+    returns (bytes memory beforeExecutionData)
+  {
+    uint256 index = abi.decode(actionData.validatorData, (uint256));
+
+    ZapOutUniswapV4ValidationData memory validationData =
+      abi.decode(coreData.validationData, (ZapOutUniswapV4ValidationData));
+
+    IPositionManager positionManager = IPositionManager(validationData.nftAddresses[index]);
+    uint256 tokenId = validationData.nftIds[index];
+    address outputToken = validationData.outputTokens[index];
+
+    IKSSessionIntentRouter.ERC721Data[] calldata erc721Data = actionData.tokenData.erc721Data;
+    require(erc721Data[0].token == address(positionManager), InvalidTokenData());
+    require(erc721Data[0].tokenId == tokenId, InvalidTokenData());
+
+    bytes32 poolId;
+    {
+      (PoolKey memory poolKey,) = positionManager.getPoolAndPositionInfo(tokenId);
+      poolId = _getPoolId(poolKey);
+    }
+
+    (uint160 sqrtPriceX96,,,) = positionManager.poolManager().getSlot0(poolId);
+    require(
+      sqrtPriceX96 >= validationData.sqrtPLowers[index]
+        && sqrtPriceX96 <= validationData.sqrtPUppers[index],
+      OutsidePriceRange(
+        validationData.sqrtPLowers[index], validationData.sqrtPUppers[index], sqrtPriceX96
+      )
+    );
+
+    uint256 liquidityBefore = positionManager.getPositionLiquidity(tokenId);
+    uint256 tokenBalanceBefore = outputToken == ETH_ADDRESS
+      ? validationData.recipient.balance
+      : IERC20(outputToken).balanceOf(validationData.recipient);
+
+    return abi.encode(
+      positionManager,
+      tokenId,
+      outputToken,
+      liquidityBefore,
+      tokenBalanceBefore,
+      validationData.minRates[index]
+    );
+  }
+
+  /// @inheritdoc IKSSessionIntentValidator
+  function validateAfterExecution(
+    bytes32,
+    IKSSessionIntentRouter.IntentCoreData calldata coreData,
+    bytes calldata beforeExecutionData,
+    bytes calldata
+  ) external view override {
+    uint256 minRate;
+    uint256 liquidity;
+    uint256 outputAmount;
+    ZapOutUniswapV4ValidationData memory validationData =
+      abi.decode(coreData.validationData, (ZapOutUniswapV4ValidationData));
+
+    {
+      IPositionManager positionManager;
+      uint256 tokenId;
+      address outputToken;
+      uint256 liquidityBefore;
+      uint256 tokenBalanceBefore;
+
+      (positionManager, tokenId, outputToken, liquidityBefore, tokenBalanceBefore, minRate) = abi
+        .decode(beforeExecutionData, (IPositionManager, uint256, address, uint256, uint256, uint256));
+
+      uint256 liquidityAfter = positionManager.getPositionLiquidity(tokenId);
+      liquidity = liquidityBefore - liquidityAfter;
+
+      outputAmount = outputToken == ETH_ADDRESS
+        ? validationData.recipient.balance - tokenBalanceBefore
+        : IERC20(outputToken).balanceOf(validationData.recipient);
+      outputAmount -= tokenBalanceBefore;
+    }
+
+    if (outputAmount * RATE_DENOMINATOR < minRate * liquidity) {
+      revert BelowMinRate(liquidity, minRate, outputAmount);
+    }
+  }
+
+  function _getPoolId(PoolKey memory poolKey) internal pure returns (bytes32 poolId) {
+    assembly {
+      poolId := keccak256(poolKey, 0xa0)
+    }
+  }
+}
