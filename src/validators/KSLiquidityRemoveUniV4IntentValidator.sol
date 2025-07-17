876afe7d1097d692a0dc985ab61c4c4a15acd046
minhtr09 <22521523@gm.uit.edu.vn>
2025-07-17 13:35:50 +0700

    fix: resolve cmts

--- src/validators/KSLiquidityRemoveUniV4IntentValidator.sol
+++ src/validators/KSLiquidityRemoveUniV4IntentValidator.sol
@@ -24,42 +24,22 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
 
   uint256 public constant OUTPUT_TOKENS = 2;
   uint256 public constant YIELD_BPS = 10_000;
-  uint256 public constant MAX_TIME_DIFFERENCE = 60; // 1 minute
   uint256 public constant Q192 = 1 << 192;
 
+  // the time condition is already validated in the router
   enum ConditionType {
-    TIME_BASED,
     YIELD_BASED,
     PRICE_BASED
   }
 
-  enum LogicOperator {
-    AND,
-    OR
-  }
-
-  enum TimeCondition {
-    NO_CONDITION,
-    BEFORE,
-    AT,
-    AFTER
-  }
-
   struct Condition {
     ConditionType conditionType;
-    bool isActive;
     bytes conditionData;
-  }
-
-  struct TimeBasedCondition {
-    TimeCondition timeType;
-    uint256 targetTimestamp;
   }
 
   struct YieldBasedCondition {
     uint256 targetYieldBps; // Basis points (10000 = 100%)
     uint256 initialAmounts; // [token0, token1]
-    uint256 initialFees; // [fee0, fee1]
   }
 
   struct PriceBasedCondition {
@@ -67,10 +47,23 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
     uint160 maxSqrtPrice;
   }
 
+  /**
+   * @dev 2D array for logical expressions represented in Disjunctive Normal Form
+   * Example: (A and B) or (C and D and E) = [[A,B], [C,D,E]]
+   */
   struct ConditionData {
-    Condition condition1;
-    Condition condition2;
-    LogicOperator logicOperator;
+    Condition[][] conditions;
+  }
+
+  struct LocalVar {
+    address recipient;
+    IPositionManager positionManager;
+    uint256 tokenId;
+    address[] outputTokens;
+    uint256[] minAmountsOut;
+    uint256 liquidityBefore;
+    uint256[] tokenBalanceBefore;
+    uint160 sqrtPriceX96;
   }
 
   struct ZapOutUniswapV4ValidationData {
@@ -101,72 +94,16 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
     checkTokenLengths(actionData.tokenData)
     returns (bytes memory beforeExecutionData)
   {
-    (uint256 index, uint256 feesCollected) =
-      abi.decode(actionData.validatorData, (uint256, uint256));
+    (uint256 index, uint256 feesCollected) = _decodeValidatorData(actionData.validatorData);
+    // to avoid stack too deep
+    LocalVar memory localVar;
+    Condition[][] calldata conditions =
+      _cacheAndDecodeValidationData(coreData.validationData, localVar, index);
 
-    ZapOutUniswapV4ValidationData memory validationData =
-      abi.decode(coreData.validationData, (ZapOutUniswapV4ValidationData));
+    _validateTokenData(actionData.tokenData, localVar);
+    _validateConditions(conditions, feesCollected, localVar.sqrtPriceX96);
 
-    IPositionManager positionManager = IPositionManager(validationData.nftAddresses[index]);
-    uint256 tokenId = validationData.nftIds[index];
-    address[] memory outputTokens = validationData.outputTokens[index];
-    uint256[] memory minAmountsOut = validationData.minAmountsOut[index];
-    ConditionData memory condition = validationData.conditions[index];
-
-    require(
-      outputTokens[0] != outputTokens[1] && outputTokens[0] < outputTokens[1]
-        && outputTokens.length == OUTPUT_TOKENS && outputTokens.length == minAmountsOut.length,
-      InvalidOutputToken()
-    );
-
-    IKSSessionIntentRouter.ERC721Data[] calldata erc721Data = actionData.tokenData.erc721Data;
-    require(erc721Data[0].token == address(positionManager), InvalidTokenData());
-    require(erc721Data[0].tokenId == tokenId, InvalidTokenData());
-
-    uint256[] memory unclaimedFees = new uint256[](2);
-    uint160 sqrtPriceX96;
-    {
-      IPoolManager poolManager = positionManager.poolManager();
-
-      (PoolKey memory poolKey, uint256 positionInfo) =
-        positionManager.getPoolAndPositionInfo(tokenId);
-      bytes32 poolId = _getPoolId(poolKey);
-      int24 tickCurrent;
-      (sqrtPriceX96, tickCurrent,,) = poolManager.getSlot0(poolId);
-      (int24 tickLower, int24 tickUpper) = _getTickRange(positionInfo);
-
-      (unclaimedFees[0], unclaimedFees[1]) = _computeUnclaimedFees(
-        poolManager,
-        address(positionManager),
-        poolId,
-        tickLower,
-        tickUpper,
-        tickCurrent,
-        bytes32(tokenId)
-      );
-    }
-
-    require(
-      _validateConditions(condition, feesCollected, unclaimedFees, outputTokens, sqrtPriceX96),
-      ConditionsNotMet()
-    );
-
-    uint256 liquidityBefore = positionManager.getPositionLiquidity(tokenId);
-    require(liquidityBefore > 0, InvalidLiquidity());
-    uint256[] memory tokenBalanceBefore = new uint256[](outputTokens.length);
-    for (uint256 i = 0; i < outputTokens.length; i++) {
-      tokenBalanceBefore[i] = outputTokens[i].balanceOf(validationData.recipient);
-    }
-
-    return abi.encode(
-      positionManager,
-      tokenId,
-      outputTokens,
-      liquidityBefore,
-      tokenBalanceBefore,
-      minAmountsOut,
-      validationData.recipient
-    );
+    return abi.encode(localVar);
   }
 
   /// @inheritdoc IKSSessionIntentValidator
@@ -176,125 +113,94 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
     bytes calldata beforeExecutionData,
     bytes calldata
   ) external view override {
-    (
-      IPositionManager positionManager,
-      uint256 tokenId,
-      address[] memory outputTokens,
-      uint256 liquidityBefore,
-      uint256[] memory tokenBalanceBefore,
-      uint256[] memory minAmountsOut,
-      address recipient
-    ) = abi.decode(
-      beforeExecutionData,
-      (IPositionManager, uint256, address[], uint256, uint256[], uint256[], address)
-    );
+    LocalVar calldata localVar = _decodeBeforeExecutionData(beforeExecutionData);
 
-    uint256 liquidityAfter = positionManager.getPositionLiquidity(tokenId);
+    uint256 liquidityAfter = localVar.positionManager.getPositionLiquidity(localVar.tokenId);
     require(
-      liquidityAfter == 0 || positionManager.ownerOf(tokenId) == coreData.mainAddress,
-      InvalidOwner()
+      localVar.positionManager.ownerOf(localVar.tokenId) == coreData.mainAddress, InvalidOwner()
     );
-    uint256 liquidityRemoved = liquidityBefore - liquidityAfter;
+    uint256 liquidityRemoved = localVar.liquidityBefore - liquidityAfter;
     require(liquidityRemoved > 0, InvalidLiquidity());
 
-    uint256[] memory outputAmounts = new uint256[](outputTokens.length);
-    for (uint256 i = 0; i < outputTokens.length; i++) {
-      outputAmounts[i] = outputTokens[i].balanceOf(recipient) - tokenBalanceBefore[i];
+    uint256[] memory outputAmounts = new uint256[](localVar.outputTokens.length);
+    for (uint256 i = 0; i < localVar.outputTokens.length; i++) {
+      outputAmounts[i] =
+        localVar.outputTokens[i].balanceOf(localVar.recipient) - localVar.tokenBalanceBefore[i];
     }
+    _validateOutputAmounts(outputAmounts, localVar.minAmountsOut);
+  }
 
-    _validateOutputAmounts(outputAmounts, minAmountsOut);
+  function _validateTokenData(
+    IKSSessionIntentRouter.TokenData calldata tokenData,
+    LocalVar memory localVar
+  ) internal view {
+    require(tokenData.erc20Data.length == 0, InvalidTokenData());
+    require(tokenData.erc721Data.length == 1, InvalidTokenData());
+    require(
+      localVar.outputTokens[0] != localVar.outputTokens[1]
+        && localVar.outputTokens[0] < localVar.outputTokens[1]
+        && localVar.outputTokens.length == OUTPUT_TOKENS
+        && localVar.outputTokens.length == localVar.minAmountsOut.length,
+      InvalidOutputToken()
+    );
+    require(localVar.liquidityBefore > 0, InvalidLiquidity());
   }
 
   function _validateConditions(
-    ConditionData memory condition,
+    Condition[][] calldata conditions,
     uint256 feesCollected,
-    uint256[] memory unclaimedFees,
-    address[] memory outputTokens,
     uint160 sqrtPriceX96
-  ) internal view returns (bool) {
-    bool condition1Met;
-    bool condition2Met;
+  ) internal pure {
+    if (conditions.length == 0) return;
 
-    if (condition.condition1.isActive) {
-      condition1Met = _evaluateCondition(
-        condition.condition1, feesCollected, unclaimedFees, outputTokens, sqrtPriceX96
-      );
+    // each condition is a disjunction (or) of conjunctions (and)
+    for (uint256 i; i < conditions.length; ++i) {
+      if (_evaluateConjunction(conditions[i], feesCollected, sqrtPriceX96)) {
+        return;
+      } else {
+        // if false, continue to the next condition
+        continue;
+      }
     }
 
-    if (condition.condition2.isActive) {
-      condition2Met = _evaluateCondition(
-        condition.condition2, feesCollected, unclaimedFees, outputTokens, sqrtPriceX96
-      );
-    }
-
-    if (condition.logicOperator == LogicOperator.AND) {
-      return condition1Met && condition2Met;
-    } else if (condition.logicOperator == LogicOperator.OR) {
-      return condition1Met || condition2Met;
-    }
-
-    return false;
+    revert ConditionsNotMet();
   }
 
-  function _evaluateCondition(
-    Condition memory condition,
+  function _evaluateConjunction(
+    Condition[] memory conjunction,
     uint256 feesCollected,
-    uint256[] memory unclaimedFees,
-    address[] memory outputTokens,
     uint160 sqrtPriceX96
-  ) internal view returns (bool) {
-    if (condition.conditionType == ConditionType.TIME_BASED) {
-      return _evaluateTimeCondition(condition.conditionData);
-    } else if (condition.conditionType == ConditionType.YIELD_BASED) {
-      return _evaluateYieldCondition(
-        condition.conditionData, feesCollected, unclaimedFees, outputTokens, sqrtPriceX96
-      );
-    } else if (condition.conditionType == ConditionType.PRICE_BASED) {
-      return _evaluatePriceCondition(condition.conditionData, sqrtPriceX96);
-    }
-    return false;
-  }
+  ) internal pure returns (bool) {
+    if (conjunction.length == 0) return true;
 
-  function _evaluateTimeCondition(bytes memory conditionData) internal view returns (bool) {
-    TimeBasedCondition memory timeCondition = abi.decode(conditionData, (TimeBasedCondition));
-
-    if (timeCondition.timeType == TimeCondition.NO_CONDITION) {
-      return true;
-    } else if (timeCondition.timeType == TimeCondition.BEFORE) {
-      return block.timestamp < timeCondition.targetTimestamp;
-    } else if (timeCondition.timeType == TimeCondition.AT) {
-      return abs(block.timestamp, timeCondition.targetTimestamp) <= MAX_TIME_DIFFERENCE;
-    } else if (timeCondition.timeType == TimeCondition.AFTER) {
-      return block.timestamp > timeCondition.targetTimestamp;
+    bool meetCondition;
+    for (uint256 i = 0; i < conjunction.length; i++) {
+      if (conjunction[i].conditionType == ConditionType.YIELD_BASED) {
+        meetCondition =
+          _evaluateYieldCondition(conjunction[i].conditionData, feesCollected, sqrtPriceX96);
+      } else if (conjunction[i].conditionType == ConditionType.PRICE_BASED) {
+        meetCondition = _evaluatePriceCondition(conjunction[i].conditionData, sqrtPriceX96);
+      }
+      if (!meetCondition) {
+        return false;
+      }
     }
-    return false;
+    return true;
   }
 
   function _evaluateYieldCondition(
     bytes memory conditionData,
     uint256 feesCollected,
-    uint256[] memory fees,
-    address[] memory outputTokens,
     uint160 sqrtPriceX96
-  ) internal view returns (bool) {
+  ) internal pure returns (bool) {
     YieldBasedCondition memory yieldCondition = abi.decode(conditionData, (YieldBasedCondition));
-    uint8 decimals0 = IERC20Metadata(outputTokens[0]).decimals();
-    uint8 decimals1 = IERC20Metadata(outputTokens[1]).decimals();
-    {
-      uint256 fee0Collected = feesCollected >> 128;
-      uint256 fee1Collected = uint256(uint128(feesCollected));
-      uint256 initialFee0 = yieldCondition.initialFees >> 128;
-      uint256 initialFee1 = uint256(uint128(yieldCondition.initialFees));
+    uint256 fee0Collected = feesCollected >> 128;
+    uint256 fee1Collected = uint256(uint128(feesCollected));
 
-      fees[0] = _scaleTo18Decimals(fees[0] + fee0Collected - initialFee0, decimals0);
-      fees[1] = _scaleTo18Decimals(fees[1] + fee1Collected - initialFee1, decimals1);
-    }
+    uint256 initialAmount0 = yieldCondition.initialAmounts >> 128;
+    uint256 initialAmount1 = uint256(uint128(yieldCondition.initialAmounts));
 
-    uint256 initialAmount0 = _scaleTo18Decimals(yieldCondition.initialAmounts >> 128, decimals0);
-    uint256 initialAmount1 =
-      _scaleTo18Decimals(uint256(uint128(yieldCondition.initialAmounts)), decimals1);
-
-    uint256 numerator = fees[0] + _convertToken1ToToken0(sqrtPriceX96, fees[1]);
+    uint256 numerator = fee0Collected + _convertToken1ToToken0(sqrtPriceX96, fee1Collected);
     uint256 denominator = initialAmount0 + _convertToken1ToToken0(sqrtPriceX96, initialAmount1);
     uint256 yieldBps = (numerator * YIELD_BPS) / denominator;
 
@@ -330,24 +236,11 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
     }
   }
 
-  function _getTickRange(uint256 posInfo)
-    internal
-    pure
-    returns (int24 _tickLower, int24 _tickUpper)
-  {
-    assembly {
-      _tickLower := signextend(2, shr(8, posInfo))
-      _tickUpper := signextend(2, shr(32, posInfo))
-    }
-  }
-
   function _convertToken1ToToken0(uint256 sqrtPriceX96, uint256 amount1)
     internal
     pure
     returns (uint256 amount0)
   {
-    require(sqrtPriceX96 > 0, 'sqrtPriceX96 must be > 0');
-
     // Calculate (sqrtPriceX96)^2
     uint256 sqrtPriceX96Squared = sqrtPriceX96 * sqrtPriceX96;
 
@@ -355,85 +248,55 @@ contract KSLiquidityRemoveUniV4IntentValidator is BaseIntentValidator {
     amount0 = (amount1 * Q192) / sqrtPriceX96Squared;
   }
 
-  function _scaleTo18Decimals(uint256 amount, uint8 decimals) internal pure returns (uint256) {
-    if (decimals == 18) {
-      return amount;
-    } else if (decimals < 18) {
-      return amount * (10 ** (18 - decimals));
-    } else {
-      return amount / (10 ** (decimals - 18));
+  function _decodeValidatorData(bytes calldata data)
+    internal
+    pure
+    returns (uint256 index, uint256 feesCollected)
+  {
+    assembly ("memory-safe") {
+      index := calldataload(data.offset)
+      feesCollected := calldataload(add(data.offset, 0x20))
     }
   }
 
-  function _computeUnclaimedFees(
-    IPoolManager poolManager,
-    address owner,
-    bytes32 poolId,
-    int24 tickCurrent,
-    int24 tickLower,
-    int24 tickUpper,
-    bytes32 salt
-  ) internal view returns (uint256 feesOwed0, uint256 feesOwed1) {
-    bytes32 positionKey = StateLibrary.calculatePositionKey(owner, tickLower, tickUpper, salt);
+  function _cacheAndDecodeValidationData(
+    bytes calldata data,
+    LocalVar memory localVar,
+    uint256 index
+  ) internal view returns (Condition[][] calldata conditions) {
+    ZapOutUniswapV4ValidationData calldata validationData;
+    assembly ("memory-safe") {
+      validationData := add(data.offset, calldataload(add(data.offset, 0x20)))
+    }
+    conditions = validationData.conditions[index].conditions;
 
-    (uint128 liquidity, uint256 feeGrowthInside0Last, uint256 feeGrowthInside1Last) =
-      poolManager.getPositionInfo(poolId, positionKey);
-    uint256 Q128 = 1 << 128;
+    localVar.recipient = validationData.recipient;
+    localVar.positionManager = IPositionManager(validationData.nftAddresses[index]);
+    localVar.tokenId = validationData.nftIds[index];
+    localVar.outputTokens = validationData.outputTokens[index];
+    localVar.minAmountsOut = validationData.minAmountsOut[index];
+    localVar.liquidityBefore = localVar.positionManager.getPositionLiquidity(localVar.tokenId);
+    localVar.tokenBalanceBefore = new uint256[](localVar.outputTokens.length);
+    for (uint256 i; i < localVar.outputTokens.length; ++i) {
+      localVar.tokenBalanceBefore[i] = localVar.outputTokens[i].balanceOf(localVar.recipient);
+    }
 
-    (uint256 feeGrowthInside0, uint256 feeGrowthInside1) =
-      _getFeeGrowthInside(poolManager, poolId, tickLower, tickUpper, tickCurrent);
+    {
+      IPoolManager poolManager = localVar.positionManager.poolManager();
 
-    unchecked {
-      feesOwed0 = FullMath.mulDivFloor(feeGrowthInside0 - feeGrowthInside0Last, liquidity, Q128);
-      feesOwed1 = FullMath.mulDivFloor(feeGrowthInside1 - feeGrowthInside1Last, liquidity, Q128);
+      (PoolKey memory poolKey,) = localVar.positionManager.getPoolAndPositionInfo(localVar.tokenId);
+      bytes32 poolId = _getPoolId(poolKey);
+      (localVar.sqrtPriceX96,,,) = poolManager.getSlot0(poolId);
     }
   }
 
-  function _getFeeGrowthInside(
-    IPoolManager poolManager,
-    bytes32 poolId,
-    int24 tickLower,
-    int24 tickUpper,
-    int24 tickCurrent
-  ) internal view returns (uint256 feeGrowthInside0X128, uint256 feeGrowthInside1X128) {
-    (uint256 feeGrowthGlobal0X128, uint256 feeGrowthGlobal1X128) =
-      poolManager.getFeeGrowthGlobals(poolId);
-    (uint256 feeGrowthOutside0X128Lower, uint256 feeGrowthOutside1X128Lower) =
-      poolManager.getTickFeeGrowthOutside(poolId, tickLower);
-    (uint256 feeGrowthOutside0X128Upper, uint256 feeGrowthOutside1X128Upper) =
-      poolManager.getTickFeeGrowthOutside(poolId, tickUpper);
-
-    uint256 feeGrowthBelow0X128;
-    uint256 feeGrowthBelow1X128;
-    unchecked {
-      if (tickCurrent >= tickLower) {
-        feeGrowthBelow0X128 = feeGrowthOutside0X128Lower;
-        feeGrowthBelow1X128 = feeGrowthOutside1X128Lower;
-      } else {
-        feeGrowthBelow0X128 = feeGrowthGlobal0X128 - feeGrowthOutside0X128Lower;
-        feeGrowthBelow1X128 = feeGrowthGlobal1X128 - feeGrowthOutside1X128Lower;
-      }
-
-      uint256 feeGrowthAbove0X128;
-      uint256 feeGrowthAbove1X128;
-      if (tickCurrent < tickUpper) {
-        feeGrowthAbove0X128 = feeGrowthOutside0X128Upper;
-        feeGrowthAbove1X128 = feeGrowthOutside1X128Upper;
-      } else {
-        feeGrowthAbove0X128 = feeGrowthGlobal0X128 - feeGrowthOutside0X128Upper;
-        feeGrowthAbove1X128 = feeGrowthGlobal1X128 - feeGrowthOutside1X128Upper;
-      }
-
-      feeGrowthInside0X128 = feeGrowthGlobal0X128 - feeGrowthBelow0X128 - feeGrowthAbove0X128;
-      feeGrowthInside1X128 = feeGrowthGlobal1X128 - feeGrowthBelow1X128 - feeGrowthAbove1X128;
+  function _decodeBeforeExecutionData(bytes calldata data)
+    internal
+    pure
+    returns (LocalVar calldata localVar)
+  {
+    assembly ("memory-safe") {
+      localVar := add(data.offset, calldataload(add(data.offset, 0x20)))
     }
-  }
-
-  function _sqrtPriceX96ToPrice(uint160 sqrtPriceX96) internal pure returns (uint256) {
-    return (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) >> 192;
-  }
-
-  function abs(uint256 a, uint256 b) internal pure returns (uint256) {
-    return a >= b ? a - b : b - a;
   }
 }
