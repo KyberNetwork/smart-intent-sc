7a8532d4c0f0df872220f0675958991e73d153f7
minhtr09 <22521523@gm.uit.edu.vn>
2025-10-22 17:46:32 +0700

    fix: remove array indexed

--- src/interfaces/IKSSmartIntentRouter.sol
+++ src/interfaces/IKSSmartIntentRouter.sol
@@ -70,7 +70,7 @@ interface IKSSmartIntentRouter {
   event RecordVolumeAndFees(
     address indexed token,
     address indexed protocolRecipient,
-    address[] indexed partnerRecipients,
+    address[] partnerRecipients,
     uint256 protocolFeeAmount,
     uint256[] partnersFeeAmounts,
     bool beforeExecution,
